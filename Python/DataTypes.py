9e66523ae7c4cba7574bf5d042eacbcfea696a93
Mehul Kanojiya <66484553+idle-programmer@users.noreply.github.com>
2025-10-06 17:41:43 +0530

    Add files via upload

---
+++ Python/DataTypes.py
@@ -0,0 +1,37 @@
+"""Numeric Types : Integer, Float, Complex Numbers"""
+
+int
+x = 8  # immutable
+float
+y = 1.8  # immutable
+complex
+z = 1j
+
+"""Sequence Types : List, Tuple, Range, String (also text type)"""
+list
+a = [1, 2, 3, 4]
+tuple
+b = (1, 2, 3, 4)  # immutable
+range
+c = range(4)
+str
+d = "my string"
+
+"""Mapping Types : Dictionary"""
+dict
+e = {"key": "value"}
+
+"""Set Types : Set, frozenset"""
+set
+f = {"a", "b", "c"}
+frozenset
+g = frozenset({"a", "b", "c"})
+
+"""Boolean Type : Bool"""
+h = True / False
+
+"""Binary Type : bytes"""
+i = b"jkl"
+
+"""None Type : bytes"""
+m = None
