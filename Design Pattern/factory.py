3f2b2993978bb96262d1eab41b65dd770cc7e516
idle-programmer <66484553+idle-programmer@users.noreply.github.com>
2025-11-11 13:00:51 +0530

    Factory

---
+++ Design Pattern/factory.py
@@ -0,0 +1,34 @@
+"""
+The Factory Method Pattern is used to create objects without exposing the instantiation logic to the client.
+It defines an interface for creating an object, but allows subclasses or factories to decide which class to instantiate.
+For example:
+1) If we need to create multiple notification types (SMS, Email, Push), we can use a factory method to return the
+appropriate notification object based on user input.
+2) Payment gateways (Razorpay, Stripe, etc.)
+3) Database drivers (MySQL, PostgreSQL, SQLite)
+"""
+
+
+class MySQLdb:
+    def connect(self):
+        print("Connecting to MySQL Database")
+
+
+class PostgresSQLdb:
+    def connect(self):
+        print("Connecting to PostresSQL Database")
+
+
+class DatabaseFactory:
+    @staticmethod
+    def get_database(db_type):
+        if db_type == "mysql":
+            return MySQLdb()
+        elif db_type == "postgressql":
+            return PostgresSQLdb()
+        else:
+            raise ValueError("Unsupported database type")
+
+
+db = DatabaseFactory.get_database("mysql")
+db.connect()
