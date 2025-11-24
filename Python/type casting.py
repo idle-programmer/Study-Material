1a3988daa3b907b482f266e8dffa66bccd08ba4c
idle-programmer <66484553+idle-programmer@users.noreply.github.com>
2025-11-24 17:37:30 +0530

    type cast

---
+++ Python/type casting.py
@@ -0,0 +1,17 @@
+int()
+float()
+str()
+list()
+set()
+dict()
+tuple()
+bool()
+complex()
+
+# Interview Questions
+# implicit vs explicit type conversion with examples
+"""
+explicit : done by us, eg: int("3")
+implicit : done by python, eg: *args is tupple, **args is dict.
+"""
+
