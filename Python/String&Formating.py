6dc7618ea25b44d1b9b4f6f83d83bae8fb620ce6
idle-programmer <66484553+idle-programmer@users.noreply.github.com>
2025-11-26 11:08:34 +0530

    String

---
+++ Python/String&Formating.py
@@ -0,0 +1,31 @@
+example = "Hello, World!"
+
+# ----Slicing---- #
+print(example[2:5])  # starting and ending index, end position will be x-1
+
+print(example[:5])  # non starting value will start from 0
+
+print(example[2:])  # non ending value will traval till end of string
+
+print(example[-5:-2])  # "-" negative indexing works for reverse slicing
+
+
+
+
+# ----Modify Strings---- #
+print(example.upper())
+
+print(example.lower())
+
+# removes space from start and end of string, also has lstrip and rstrip
+print(example.strip())
+
+# replace the given char of string,
+print(example.replace("H", "B"))
+# Replace the two first occurrence of the word if number given
+print(example.replace("l", "B", 2))
+
+print(example.split(","))  # created a list of text between the given seperator,
+
+# if number given after seperator it will only split to the given number
+print(example.split(",", 0))
