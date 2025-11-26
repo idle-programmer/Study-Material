76570cd5f36c7645fad4634fca0822cf37d44a5c
idle-programmer <66484553+idle-programmer@users.noreply.github.com>
2025-11-26 12:42:16 +0530

    .

--- Python/String&Formating.py
+++ Python/String&Formating.py
@@ -10,8 +10,6 @@ print(example[2:])  # non ending value will traval till end of string
 print(example[-5:-2])  # "-" negative indexing works for reverse slicing
 
 
-
-
 # ----Modify Strings---- #
 print(example.upper())
 
@@ -22,6 +20,7 @@ print(example.strip())
 
 # replace the given char of string,
 print(example.replace("H", "B"))
+
 # Replace the two first occurrence of the word if number given
 print(example.replace("l", "B", 2))
 
@@ -29,3 +28,18 @@ print(example.split(","))  # created a list of text between the given seperator,
 
 # if number given after seperator it will only split to the given number
 print(example.split(",", 0))
+
+
+# ----String Concatenation---- #
+a = "Hello"
+b = "World"
+c = a + b
+
+
+# ----F-Strings---- #
+age = 36
+txt = f"My name is John, I am {age}"
+txt = f"My name is John, I am {age*10}"
+txt = f"My name is John, I am {age:.2f}"  # modifier is included by adding a colon ":"
+
+
--- Python/type casting.py
+++ Python/type casting.py
@@ -15,3 +15,11 @@ explicit : done by us, eg: int("3")
 implicit : done by python, eg: *args is tupple, **args is dict.
 """
 
+# many values that evaluate to False
+bool(False)  # return False
+bool(None)  # return False
+bool(0)  # return False
+bool("")  # return False
+bool(())  # return False
+bool([])  # return False
+bool({})  # return False
